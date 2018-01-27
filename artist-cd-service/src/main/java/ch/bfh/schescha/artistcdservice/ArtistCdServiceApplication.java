b6d37c0e9e4a3b43d0847368e88add40f9b1dbe7
Jan Scheidegger <scheideggerj@gmail.com>
2018-01-27 18:40:15 +0100

    Added Songs to CD

--- artist-cd-service/src/main/java/ch/bfh/schescha/artistcdservice/ArtistCdServiceApplication.java
+++ artist-cd-service/src/main/java/ch/bfh/schescha/artistcdservice/ArtistCdServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
@@ -12,7 +13,6 @@ import org.springframework.hateoas.config.EnableHypermediaSupport;
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
-@EnableHystrixDashboard
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class ArtistCdServiceApplication {
 
